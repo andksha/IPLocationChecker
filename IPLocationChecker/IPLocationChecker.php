c5319813586dbacfe154e86d1dd5689a611f91b8
andrew <andrewafk2000@gmail.com>
2018-10-19 17:48:32 +0300

    Fixed issue with incrementing IPs array index.

--- IPLocationChecker/IPLocationChecker.php
+++ IPLocationChecker/IPLocationChecker.php
@@ -10,9 +10,9 @@ class IPChecker
         $this->setIPs($pdo);
     }
 
-    public function setIPs($pdo)
+    public function setIPs($pdo, $query = 'SELECT ip, country FROM tb_ip_addresses WHERE 1')
     {
-        $this->ips = $pdo->query('SELECT ip, country FROM tb_ip_addresses WHERE 1');
+        $this->ips = $pdo->query($query)->fetchAll();
     }
 
     public function getIPs()
@@ -26,15 +26,14 @@ class IPChecker
 
         foreach ($this->ips as $ip) {
             array_push($responses, json_decode(file_get_contents(
-                'http://api.ipapi.com/' . $ip['ip'] . '?access_key=MY_ACCESS_KEY'
+                'http://api.ipapi.com/' . $ip['ip'] . '?access_key=0889ed3339a8d20ce19394ee31e045e0'
             )));
         }
 
         $sql  = "UPDATE tb_ip_addresses SET country=? WHERE ip=?";
         $stmt = $pdo->prepare($sql);
-
         foreach ($responses as $key => $response ) {
-            if ($response->success == true) {
+            if ($response->country_name != $this->ips[$key]['country']) {
                 $updateResult = $stmt->execute([$response->country_name, $response->ip]);
             } else {
                 $updateResult = false;
